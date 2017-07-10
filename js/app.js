7d0660a4eed21ba4402fa9c39793cdd013efe30e
shad-k <shadab.2005@gmail.com>
2017-07-10 17:58:51 +0530

    Organize code into modules

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
 $(function() {
-	var sectionTop = $("#section2").offset().top;
-	sectionTop -= $(".nav").height();
-	var sectionBottom = $("#section2").height() + sectionTop;
+	var Data = {
+		init: function() {
 
-	$(window).scroll(function() {
-		var scroll = $(window).scrollTop();
-		if(scroll < sectionTop ) {
-			if ($(".navElement").hasClass("animate")) {
-				$(".navElement").removeClass("animate").addClass("unanimate");
-			}
-		} else if(scroll > sectionBottom) {
-			if ($(".navElement").hasClass("animate")) {
-				$(".navElement").removeClass("animate").addClass("unanimate");
-			}
-		} else if(scroll > sectionTop) {
-			if ($(".navElement").hasClass("unanimate")) {
-				$(".navElement").removeClass("unanimate");
-			}
-			$(".navElement").addClass("animate");
 		}
-	});
+	};
+
+	var App = {
+		init: function() {
+			Data.init();
+			View.init();
+		}
+	}
+
+	var View = {
+		init: function() {
+			var sectionTop = $("#section2").offset().top;
+			sectionTop -= $(".nav").height();
+			var sectionBottom = $("#section2").height() + sectionTop;
+
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if(scroll < sectionTop ) {
+					if ($(".navElement").hasClass("animate")) {
+						$(".navElement").removeClass("animate").addClass("unanimate");
+					}
+				} else if(scroll > sectionBottom) {
+					if ($(".navElement").hasClass("animate")) {
+						$(".navElement").removeClass("animate").addClass("unanimate");
+					}
+				} else if(scroll > sectionTop) {
+					if ($(".navElement").hasClass("unanimate")) {
+						$(".navElement").removeClass("unanimate");
+					}
+					$(".navElement").addClass("animate");
+				}
+			});
+		}
+	}
+
+	App.init();
 });
