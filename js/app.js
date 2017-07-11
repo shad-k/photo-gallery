4e94bcb861f0d647e9dc00602ae9762f2f0134c6
shad-k <shadab.2005@gmail.com>
2017-07-11 10:26:54 +0530

    Add scroll animation on nav bar click

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ $(function() {
 
 	var View = {
 		init: function() {
+			this.setCustomScrollSpy();
+			this.setClickOnNav();
+		},
+		setCustomScrollSpy: function() {
 			var sectionTop = $("#section2").offset().top;
 			sectionTop -= $(".nav").height();
 			var sectionBottom = $("#section2").height() + sectionTop;
@@ -23,23 +27,38 @@ $(function() {
 				if(scroll < sectionTop ) {
 					if ($(".navElement").hasClass("animate")) {
 						$(".navElement").removeClass("animate").addClass("unanimate");
-						$(".navText").css("color", "#000");
 					}
 				} else if(scroll > sectionBottom) {
 					if ($(".navElement").hasClass("animate")) {
 						$(".navElement").removeClass("animate").addClass("unanimate");
-						$(".navText").css("color", "#000");
 					}
 				} else if(scroll > sectionTop) {
 					if ($(".navElement").hasClass("unanimate")) {
 						$(".navElement").removeClass("unanimate");
 					}
 					$(".navElement").addClass("animate");
-					$(".navText").css("color", "#FFF");
 				}
 			});
+		},
+		setClickOnNav: function() {
+			$(".home").click(function() {
+				$("html, body").animate({scrollTop: 0}, 500);
+				// $(window).scrollTop();
+			});
+
+			$(".gallery").click(function() {
+				var gallery = $("#section2").offset().top;
+				$("html, body").animate({scrollTop: gallery}, 500);
+				// $(window).scrollTop();
+			});
+
+			$(".contact").click(function() {
+				var contact = $("#section3").offset().top;
+				$("html, body").animate({scrollTop: contact}, 500);
+				// $(window).scrollTop();
+			});
 		}
-	}
+	};
 
 	App.init();
 });
